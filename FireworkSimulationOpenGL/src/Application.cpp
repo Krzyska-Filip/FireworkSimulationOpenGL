34c963e541b525c226706d0c79c372bc7e85db11
Krzyśka Filip <45741043+Krzyska-Filip@users.noreply.github.com>
2021-12-23 16:52:28 +0100

    Abstracting Classes https://www.youtube.com/watch?v=jjaTTRFXRAk&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=16

--- FireworkSimulationOpenGL/src/Application.cpp
+++ FireworkSimulationOpenGL/src/Application.cpp
@@ -8,6 +8,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "VertexArray.h"
 #include "Shader.h"
 
@@ -69,6 +70,8 @@ int main(void)
         vb.Unbind();
         ib.Unbind();
         shader.Unbind();
+        
+        Renderer renderer;
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -76,24 +79,18 @@ int main(void)
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
-
+            renderer.Clear();
+            
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             r += increment;
-            if (r > 1.0f) {
+            if (r > 1.0f)
                 increment = -0.05;
-            }
             else if (r < 0.0f)
-            {
                 increment = 0.05f;
-            }
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
